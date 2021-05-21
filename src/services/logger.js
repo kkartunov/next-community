5755751b50bb023556fb47403651ec0ce7a1e806
Kiril Kartunov <kiril.kartunov@gmail.com>
2021-05-21 10:16:07 +0300

    logger only for dev

--- src/services/logger.js
+++ src/services/logger.js
@@ -4,11 +4,15 @@
 const logger = {
   log: function() {
     const args = Array.prototype.slice.call(arguments);
-    console.log.apply(console, args);
+    if (process.env.NODE_ENV !== 'production' || process.env.NEXT_PUBLIC_ENV !== 'production') {
+      console.log.apply(console, args);
+    }
   },
   error: function() {
     const args = Array.prototype.slice.call(arguments);
-    console.error.apply(console, args);
+    if (process.env.NODE_ENV !== 'production' || process.env.NEXT_PUBLIC_ENV !== 'production') {
+      console.error.apply(console, args);
+    }
   },
 }
 
